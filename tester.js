9b732f25f209e2f108ff34f44a7bbc400072a216
e <elliotjd1@gmail.com>
2019-02-24 22:11:43 +0100

    tester now goes through a few rounds of the game

--- tester.js
+++ tester.js
@@ -1,62 +1,130 @@
+const _ = require('lodash');
+
 const Mob = require('./src/model/mob').Mob;
 const Location = require('./src/model/location').Location;
 const Action = require('./src/model/action').Action;
 const ResourceModifier = require('./src/model/resourceModifier').ResourceModifier;
+const InfluenceModifier = require('./src/model/influenceModifier').InfluenceModifier;
 
 const LocationUnownedBySource = require ('./src/model/actionRequirements/locationUnownedBySource').LocationUnownedBySource;
+const LocationOwnedBySource = require ('./src/model/actionRequirements/locationOwnedBySource').LocationOwnedBySource;
 const SourceHasMinResource = require ('./src/model/actionRequirements/sourceHasMinResource').SourceHasMinResource;
 
 
+console.log('\n\n\n=======Welcome to Penny Mob=====');
+
 const mobbo = new Mob('Mobbo');
 const theLads = new Mob('The Lads');
-mobbo.loot = 20;
-theLads.loot = 20;
+
+console.log('Creating new mob: ' + mobbo.name);
+console.log('Creating new mob: ' + theLads.name);
 
 const digs = new Location('Digs', 'Where you live.', 10);
-digs.addAction(new Action('Do Nothing', 'Relax man.'));
+console.log('Creating new Location: ' + digs.name);
 
+
+console.log('\n\n\n=======Spreading Influence=======');
 digs.spreadInfluence(mobbo, 5);
 digs.spreadInfluence(mobbo, 5);
 digs.spreadInfluence(theLads, 5);
 digs.spreadInfluence(mobbo, 5);
-digs.spreadInfluence(theLads, 5);
 digs.spreadInfluence(theLads, 5);
 digs.spreadInfluence(theLads, 5);
 digs.spreadInfluence(theLads, 5);
 digs.spreadInfluence(theLads, 5);
 
+console.log('Mobbo owns ' + mobbo.locations.length + ' locations');
+_.forEach(mobbo.locations, loc => {console.log('--' + loc.name);});
+console.log('The Lads own ' + theLads.locations.length + ' locations');
+_.forEach(theLads.locations, loc => {console.log('--' + loc.name);});
 console.log('Mobbos Influence: ' + digs.getInfluence(mobbo).amount);
 console.log('The Lads Influence: ' + digs.getInfluence(theLads).amount);
 
-console.log('Mobbos Loot: ' + mobbo.loot);
-console.log('The Lads Loot: ' + mobbo.loot);
 
-console.log('Mobbo owns ' + mobbo.locations.length + ' locations');
-console.log('The Lads own ' + theLads.locations.length + ' locations');
+console.log('\n\n\n=======Distributing Loot=======');
+mobbo.loot = 30;
+theLads.loot = 30;
+console.log('-Mobbos Loot: ' + mobbo.loot);
+console.log('-The Lads Loot: ' + mobbo.loot);
 
-const lootBoost = new ResourceModifier({lootMod: 12});
-const lootLoss = new ResourceModifier({lootMod: -5});
-const heist = new Action('Heist', 'Steal a bit of loot, sell it a ridiculous markup.');
-heist.sourceMods.push(lootBoost);
-heist.targetMods.push(lootLoss);
+
+console.log('\n\n\n=======Actions=======');
+// Create Heist
+const heist = new Action('~Heist~', 'Steal a bit of loot, sell it at a ridiculous markup.');
+heist.sourceMods.push(new ResourceModifier({lootMod: 10}));
+heist.targetMods.push(new ResourceModifier({lootMod: -5}));
 heist.requirements.push(new LocationUnownedBySource());
 heist.requirements.push(new SourceHasMinResource('loot', 10));
 digs.addAction(heist);
 
+// Create Production
+const produce = new Action('~Brew Bathtub Gin~', 'Maybe give it a good scrub first, eh?');
+produce.sourceMods.push(new ResourceModifier({lootMod: 5}));
+produce.sourceMods.push(new InfluenceModifier(2));
+produce.requirements.push(new LocationOwnedBySource());
+produce.requirements.push(new SourceHasMinResource('loot', 25));
+digs.addAction(produce);
 
-const mobboActions = digs.getMobActions(mobbo);
-const theLadsActions = digs.getMobActions(theLads);
-console.log('Mobbo actions: ' + mobboActions.length); // should have 2
-console.log('The Lads actions: ' + theLadsActions.length); // should have 1
+// Spread Influence
+const infl = new Action('~Buy the lads a round~', 'Share a pint and a few stories and people will pay attention.');
+infl.sourceMods.push(new ResourceModifier({lootMod: -1}));
+infl.sourceMods.push(new InfluenceModifier(5));
+infl.targetMods.push(new InfluenceModifier(-1));
+infl.requirements.push(new LocationUnownedBySource());
+digs.addAction(infl);
 
-// shouldn't work
-heist.execute(theLads, mobbo);
-console.log('Mobbos Loot: ' + mobbo.loot);
-console.log('The Lads Loot: ' + theLads.loot);
+// Super spread Influence
+const infl2 = new Action('~Put on a puppet show~', 'Not sure why everyone seems to enjoy these so much.');
+infl2.sourceMods.push(new ResourceModifier({lootMod: -15}));
+infl2.sourceMods.push(new InfluenceModifier(30));
+produce.requirements.push(new SourceHasMinResource('loot', 15));
+digs.addAction(infl2);
 
+let mobboActions = digs.getMobActions(mobbo);
+let theLadsActions = digs.getMobActions(theLads);
+console.log('Mobbo actions: ' + mobboActions.length);
+_.forEach(mobboActions, action => {console.log('--' + action.name);});
+console.log('The Lads actions: ' + theLadsActions.length);
+_.forEach(theLadsActions, action => {console.log('--' + action.name);});
 
+console.log('\n===Round1 - Getting in the Door===');
+infl.execute(mobbo, theLads);
+console.log('-Mobbos Loot: ' + mobbo.loot);
+console.log('-The Lads Loot: ' + theLads.loot);
+console.log('Mobbos Influence: ' + digs.getInfluence(mobbo).amount);
+console.log('The Lads Influence: ' + digs.getInfluence(theLads).amount);
 
-// should work
+console.log('\n===Round2 - The Celebration===');
+produce.execute(theLads, mobbo);
+console.log('-Mobbos Loot: ' + mobbo.loot);
+console.log('-The Lads Loot: ' + theLads.loot);
+console.log('Mobbos Influence: ' + digs.getInfluence(mobbo).amount);
+console.log('The Lads Influence: ' + digs.getInfluence(theLads).amount);
+
+console.log('\n===Round3 - The Heist===');
 heist.execute(mobbo, theLads);
-console.log('Mobbos Loot: ' + mobbo.loot);
-console.log('The Lads Loot: ' + theLads.loot);
+console.log('-Mobbos Loot: ' + mobbo.loot);
+console.log('-The Lads Loot: ' + theLads.loot);
+console.log('Mobbos Influence: ' + digs.getInfluence(mobbo).amount);
+console.log('The Lads Influence: ' + digs.getInfluence(theLads).amount);
+
+console.log('\n===Round4 - The Lads Accidentally Click End Turn A Bit Prematurely===');
+
+console.log('\n===Round5 - The Puppet Show===');
+infl2.execute(mobbo, theLads);
+console.log('-Mobbos Loot: ' + mobbo.loot);
+console.log('-The Lads Loot: ' + theLads.loot);
+console.log('Mobbo owns ' + mobbo.locations.length + ' locations');
+_.forEach(mobbo.locations, loc => {console.log('--' + loc.name);});
+console.log('The Lads own ' + theLads.locations.length + ' locations');
+_.forEach(theLads.locations, loc => {console.log('--' + loc.name);});
+console.log('Mobbos Influence: ' + digs.getInfluence(mobbo).amount);
+console.log('The Lads Influence: ' + digs.getInfluence(theLads).amount);
+
+mobboActions = digs.getMobActions(mobbo);
+theLadsActions = digs.getMobActions(theLads);
+console.log('\nNew Actions:')
+console.log('Mobbo actions: ' + mobboActions.length);
+_.forEach(mobboActions, action => {console.log('--' + action.name);});
+console.log('The Lads actions: ' + theLadsActions.length);
+_.forEach(theLadsActions, action => {console.log('--' + action.name);});
