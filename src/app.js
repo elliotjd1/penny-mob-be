4e615e40b555e6c318b8d34d8dc405c902fdec61
Elliot Danby <elliot.danby@geewa.com>
2019-03-08 14:47:50 +0100

    state is emtted on socket connection

--- src/app.js
+++ src/app.js
@@ -1,15 +1,41 @@
 const io = require('socket.io')();
 
+const GameManager = require('./model/gameManager');
+const Mob = require('./model/mob');
+const Location = require('./model/location');
+
 io.on('connection', client => { 
   console.log('connecting is happening...');
-  console.log(client);
+  const gm = loadTestGameManager();
+  gm.startGame();
+
+  // TODO send the state
+  client.emit('state', gm.getState());
+
+  client.on('locationSelect', payload => {
+    console.log('welcome to the location, we got fun but have run out of games');
+    console.log('payload');
+    client.emit('locationServe', {location: 'your mom'});
+  });
+
+  client.on('something', payload => {
+    console.log('the payload');
+    console.log(payload);
+  });
+  
+  // TODO on click territory, get the location and whatever thingers are available
 });
 
-io.on('something', client => { 
-  console.log('something is happening...');
-  console.log(client);
-});
 
 io.listen(1612);
 
-//io.emit('connection', {context: 'hey'});
+
+function loadTestGameManager() {
+  return new GameManager([
+    new Mob('Mobbo'),
+    new Mob('The Lads')
+  ], {
+    digs: new Location('digs', 'Digs', 'Where you live.', 10),
+    docks: new Location('docks', 'Docks', 'Boats \'n hoes', 5)
+  });
+}
