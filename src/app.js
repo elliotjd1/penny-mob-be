c7fdbd099f844b0f5a2afad1837f02cb0f3fb6af
Elliot Danby <elliot.danby@geewa.com>
2019-03-08 13:30:57 +0100

    setups

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 const io = require('socket.io')();
+
 io.on('connection', client => { 
+  console.log('connecting is happening...');
+  console.log(client);
+});
+
+io.on('something', client => { 
   console.log('something is happening...');
   console.log(client);
 });
+
 io.listen(1612);
 
-io.emit('connection', {context: 'hey'});
+//io.emit('connection', {context: 'hey'});
