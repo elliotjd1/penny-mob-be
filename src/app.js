d8ef450113977db77a2505b4e2c3e3db4c9581fe
Elliot Danby <elliot.danby@geewa.com>
2019-03-11 14:44:18 +0100

    unified message structure. added endTurn event

--- src/app.js
+++ src/app.js
@@ -3,28 +3,58 @@ const tesLoader = require('../data/testLoader');
 
 io.origins(['*:*']);
 
+
 io.on('connection', client => { 
   console.log('connecting is happening...');
 
+
   const gm = tesLoader.getGameManager();
   gm.startGame();
-
   client.emit('state', gm.getState());
 
+
   client.on('locationSelect', payload => {
+    console.log('fetching location information')
     console.log(payload);
-    const location = gm.getLocationDetail(payload.location);
-    client.emit('locationServe', location);
-    io.emit('state', gm.getState());
+    
+    try {
+      const location = gm.getLocationDetail(payload.location);
+      client.emit('locationServe', {status: 'success', payload: location});
+      io.emit('state', {status: 'success', payload: gm.getState()});
+    } catch (err) {
+      console.log(err);
+      io.emit('state', {status: 'error', payload: err});
+    }
   });
+
 
   client.on('locationAction', payload => {
     console.log('doing an action');
     console.log(payload);
-    gm.executeAction(payload.location, payload.action);
-    io.emit('state', gm.getState());
+
+    try {
+      gm.executeAction(payload.location, payload.action);
+      io.emit('state', {status: 'success', payload: gm.getState()});
+    } catch (err) {
+      console.log(err);
+      io.emit('state', {status: 'error', payload: err});
+    }
   });
 
+
+  client.on('endTurn', payload => {
+    console.log('ending the turn');
+    console.log(payload);
+
+    try {
+      gm.endTurn();
+      io.emit('state', {status: 'success', payload: gm.getState()});
+      // TODO tell the next person it is their turn?
+    } catch (err) {
+      console.log(err);
+      io.emit('state', {status: 'error', payload: err});
+    }
+  });
 });
 
 
