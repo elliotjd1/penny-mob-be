1164c41047661f0c8af09f37664d1daeaf17d0dc
Elliot Danby <elliot.danby@geewa.com>
2020-01-03 15:59:14 +0100

    players should now be able to create and join different games

--- src/app.js
+++ src/app.js
@@ -1,27 +1,90 @@
 const io = require('socket.io')();
+const config = require('config');
 const Room = require('./model/room');
-const room = new Room('room1', '1234'); // TODO support for multiple rooms
+const defaultRoom = config.bypassLobbyBrowser ? 'testRoom' : 'waitingRoom';
+const rooms = {
+  testRoom: new Room('testRoom', '1234')
+}
+
 
 io.origins(['*:*']);
-
-
 io.on('connection', client => { 
-  const player = client.id;
-  console.log(`establishing connection for ${player}...`);
+  const player = {
+    id: client.id,
+    room: defaultRoom,
+    mobName: null
+  }
+  console.log(`Establishing connection for ${player.id}...`);
+  client.join(defaultRoom);
 
-  room.joinRoom(player);
-  client.emit('state', {status: 'success', payload: room.gm.getState()});
+  // In testing mode, automatically start the game as soon as enough players are ready.
+  if (config.bypassLobbyBrowser) {
+    try {
+      player.mobName = rooms.testRoom.joinRoom(player.id);
+      if (rooms[player.room].readyToPlay) {
+        console.log('Automatically starting test game...');
+        rooms[player.room].startGame();
+      }
+    } catch (err) {
+      emitError(err);
+    }
+  }
+
+  emitPersonalNotification(`Welcome ${player.mobName}`);
+  emitState();
+  emitRoomNotification(`${player.mobName} has joined the room!`);
+
+  client.on('createRoom', payload => {
+    if (Object.keys(rooms).includes(payload.room)) {
+      emitError(`Room "${payload.room} already exists."`);
+      return;
+    }
+  });
+
+  client.on('joinRoom', payload => {
+    if (!Object.keys(rooms).includes(payload.room)) {
+      emitError(`Room "${payload.room} does not exist."`);
+      return;
+    }
+    console.log(`${player.id} is joining room ${payload.room}...`);
+    try {
+      player.mobName = rooms[payload.room].joinRoom(player.id);
+      player.room = payload.room;
+      client.join(player.room);
+      emitRoomNotification(`${player.mobName} has joined the room!`);
+    } catch (err) {
+      console.error(`${player.id} could not join room ${payload.room}...`);
+      emitError(err);
+    }
+
+    
+  });
+
+  client.on('leaveRoom', payload => {
+    rooms[player.room].leaveRoom(player.id);
+    client.join(defaultRoom);
+    player.room = defaultRoom;
+  });
+
+  client.on('startGame', payload => {
+
+  });
+
+  client.on('restartGame', payload => {
+    rooms[player.room].restartGame();
+    emitRoomNotification(`Game has been restarted`);
+  });
+
 
   client.on('locationSelect', payload => {
     console.log('fetching location information')
     console.log(payload);
     
     try {
-      const location = room.gm.getLocationDetail(player, payload.location);
+      const location = rooms[player.room].gm.getLocationDetail(player.id, payload.location);
       client.emit('locationServe', {status: 'success', payload: location});
     } catch (err) {
-      console.log(err);
-      client.emit('state', {status: 'error', payload: err});
+      emitError(err);
     }
   });
 
@@ -31,11 +94,10 @@ io.on('connection', client => {
     console.log(payload);
 
     try {
-      room.gm.executeAction(player, payload.location, payload.action);
-      io.emit('state', {status: 'success', payload: room.gm.getState()});
+      rooms[player.room].gm.executeAction(player.id, payload.location, payload.action);
+      emitState();
     } catch (err) {
-      console.log(err);
-      client.emit('state', {status: 'error', payload: err});
+      emitError(err);
     }
   });
 
@@ -44,14 +106,30 @@ io.on('connection', client => {
     console.log(payload);
 
     try {
-      room.gm.endTurn(player);
-      io.emit('state', {status: 'success', payload: room.gm.getState()});
-      // TODO tell the next person it is their turn?
+      rooms[player.room].gm.endTurn(player.id);
+      emitState();
+      // TODO tell the next person it is their turn? or that should be up to the FE to render?
     } catch (err) {
-      console.log(err);
-      client.emit('state', {status: 'error', payload: err});
+      emitError(err);
     }
   });
+
+  function emitPersonalNotification (message) {
+    client.emit('notification', {payload: message});
+  }
+
+  function emitRoomNotification (message) {
+    io.in(player.room).emit('notification', {payload: message});
+  }
+
+  function emitState () {
+    io.in(player.room).emit('state', {status: 'success', payload: rooms[player.room].gm.getState()});
+  }
+
+  function emitError (message) {
+    console.error(err);
+    client.emit('state', {status: 'error', payload: message});
+  }
 });
 
 
