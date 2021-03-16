ad6762ac58c168c42b7d6c0abf3a6dac5aa575ea
Elliot Danby <elliot.danby@geewa.com>
2021-03-16 11:34:57 +0100

    mob model update

--- src/model/mob.js
+++ src/model/mob.js
@@ -3,16 +3,19 @@ class Mob {
     this.id = null;
     this.name = name;
 
-    this.loot = 0;
-    this.power = 0;
-    this.heat = 0;
-
+    // Your mob
     this.thugs = 0;
     this.urchins = 0;
-    this.connections = 0;
-
-    this.locations = [];
     this.heroes = [];
+
+    // What you want
+    this.loot = 0;  // For purchases and bribes
+    this.muscle = 0; // For operations and skirmishes
+    this.influence = 0; // For infiltration and persuasion
+
+    // What you don't want
+    this.heat = 0;  // Attention from the authorities
+    this.dissent = 0; // Attention from your lieutenants
   }
 
   executeAction (action) {
